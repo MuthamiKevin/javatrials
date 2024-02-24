0c31cfbaea88ad37f94330b0270d5d7cb5c396ac
Kevin Muthami <153747395+MuthamiKN@users.noreply.github.com>
2024-02-24 12:19:32 +0300

    update

--- code/Input.java
+++ code/Input.java
@@ -1,15 +1,11 @@
 package code;
-import javax.swing.JOptionPane;
+
 
 public class Input {
     public static void main(String[] args) {
-       String name = JOptionPane.showInputDialog("Enter your name");
-       JOptionPane.showMessageDialog(null,"hello "+  name);
-       int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-       JOptionPane.showMessageDialog(null, "You are " + age + " years old");
-   
-       double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-       JOptionPane.showMessageDialog(null, "You are " + height  + " cms tall");
-    
+       double x =  -3.142;
+       double y = 9;
+       double z =Math.ceil(x);
+       System.out.println(z);
     }
 }
