30cdd51ae48cc24f8d8edf1ed42c23987fea3273
Kevin Muthami <153747395+MuthamiKN@users.noreply.github.com>
2024-02-26 19:52:05 +0300

    update

--- code/Input.java
+++ code/Input.java
@@ -1,14 +1,23 @@
 package code;
+import java.util.Scanner;
 
-public class Input {
-    public static void main(String[] args) {
-    String day = "friday";
-    switch(day){
-        case "sunday": System.out.println("its sunday");
-        break;
-        case "moday": System.out.println("its monday");
-        break;
+class Input{
+    public static void main(String[]args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter the starting number:");
+    int s = scanner.nextInt();
+    System.out.print("Enter the ending number");
+    int e = scanner.nextInt();
+    scanner.close();
+    for(int i=s; i<=e; i+=2){
+        System.out.println(i);
+      
+
     }
 
-    }
 }
+
+}
+
+
