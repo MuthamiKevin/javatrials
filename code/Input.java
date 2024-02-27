9f338648e112915da2acfba08946361d5fc303a8
Kevin Muthami <153747395+MuthamiKN@users.noreply.github.com>
2024-02-27 13:24:17 +0300

    update

--- code/Input.java
+++ code/Input.java
@@ -1,24 +1,26 @@
 package code;
 import java.util.Scanner;
-
 class Input{
     public static void main(String[]args){
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        
+ 
 
-    System.out.print("Enter the starting number:");
-    int s = scanner.nextInt();
-    System.out.print("Enter the ending number");
-    int e = scanner.nextInt();
-    scanner.close();
-    for(int i=s; i>=e; i--){
-    
-        System.out.println(i);
-      
+        System.out.print("Enter the number of rows:");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns:");
+        int columns = scanner.nextInt();
+        System.out.print("Enter the symbol to use:");
+        String symbol = scanner.next();
 
+        scanner.close();
+
+        for(int i=1; i<=rows; i++){
+            System.out.println();
+            for(int j=1; j<=columns; j++){
+                System.out.print(symbol);
+
+            }
+        }
     }
-
 }
-
-}
-
-
