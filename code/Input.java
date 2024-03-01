90cd890bef727a92ac657c72dc5e185e7c3f5d0f
Kevin Muthami <153747395+MuthamiKN@users.noreply.github.com>
2024-03-01 12:23:16 +0300

    update

--- code/Input.java
+++ code/Input.java
@@ -1,26 +1,15 @@
 package code;
-import java.util.Scanner;
 class Input{
     public static void main(String[]args){
-        Scanner scanner = new Scanner(System.in);
-        
+       
+       String[] cars = {"Lexus", "Porche","Dodger"};
+    
+        for(int i=0; i<cars.length; i++){
+            System.out.println(cars[i]);
+
+        }
  
 
-        System.out.print("Enter the number of rows:");
-        int rows = scanner.nextInt();
-        System.out.print("Enter the number of columns:");
-        int columns = scanner.nextInt();
-        System.out.print("Enter the symbol to use:");
-        String symbol = scanner.next();
-
-        scanner.close();
-
-        for(int i=1; i<=rows; i++){
-            System.out.println();
-            for(int j=1; j<=i; j++){
-                System.out.print(symbol);
-
-            }
-        }
+        
     }
 }
