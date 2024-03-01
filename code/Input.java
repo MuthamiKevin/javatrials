ac9274fd6ffe4fab82b409ae9b16dc2edcc63477
Kevin Muthami <153747395+MuthamiKN@users.noreply.github.com>
2024-03-01 12:45:49 +0300

    update

--- code/Input.java
+++ code/Input.java
@@ -2,14 +2,31 @@ package code;
 class Input{
     public static void main(String[]args){
        
-       String[] cars = {"Lexus", "Porche","Dodger"};
-    
-        for(int i=0; i<cars.length; i++){
-            System.out.println(cars[i]);
+   String[][] cars = new String[3][3];
+   cars[0][0] = "camaro";
+   cars[0][1] = "camaro";
+   cars[0][2] = "camaro";
+   cars[1][0] = "camaro";
+   cars[1][1] = "camaro";
+   cars[1][2] = "camaro";
+   cars[2][0] = "camaro";
+   cars[2][1] = "camaro";
+   cars[2][2] = "camaro";
 
+    for(int i=0; i<cars.length; i++){
+        System.out.println();
+        for(int j=0; j<cars[i].length; j++){
+
+            System.out.print(cars[i][j]+" ");
         }
- 
 
-        
+    }
+
+
+
+
+
+
+   
     }
 }
