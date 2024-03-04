d02d04a301644b7d036f6452ebfd6d8cf2beba61
Kevin Muthami <153747395+MuthamiKN@users.noreply.github.com>
2024-03-04 14:24:36 +0300

    update

---
+++ code/Main.java
@@ -0,0 +1,15 @@
+package code;
+
+public class Main {
+    public static void main(String[]args){
+        Person person = new Person();
+
+        person.name=("kevin");
+        person.age=(10);
+
+        System.out.println(person.name  + " is "  + person.age  +  " years old ");
+
+
+
+    }
+}
---
+++ code/Person.java
@@ -0,0 +1,9 @@
+package code;
+
+public class Person {
+
+    public String name;
+    public int age;
+
+    
+}
