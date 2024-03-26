eda9615d383de7540ae02ec92df2ca9328cb4ee7
Kevin Muthami <muthamikevin80@gmail.com>
2024-03-26 09:52:41 +0300

    update

--- code/divisbility.java
+++ code/divisbility.java
@@ -1,9 +1,10 @@
 package code;
 public class divisbility{
     public static void main(String []args){
-        int i;
+        int i,sum =0;
         for(i=1; i<=100; i++)
             if(i%2==0)
-            System.out.println(i);
+            sum=sum+i;
+            System.out.println(sum);
     }
 }
