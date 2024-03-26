88115dbc52ec814014b03ab9c569c1d5b347e228
Kevin Muthami <muthamikevin80@gmail.com>
2024-03-26 16:54:40 +0300

    update

--- code/divisbility.java
+++ code/divisbility.java
@@ -5,16 +5,23 @@ import java.util.Scanner;
 public class divisbility{
     public static void main(String []args){
       Scanner scanner = new Scanner(System.in);
-      int[] marks = new int[10];
-      int i;
+      int [] num = new int[10];
+      int i,avg, sum=0;
 
-      for(i=0;i<=10;i++){
-        System.out.print("Enter the element" + (i) + ":");
-        marks[i] = scanner.nextInt();
+      for(i=0; i<=9; i++){
+        System.out.println("Enter the " +  (i) + " element : " );
+        num[i] = scanner.nextInt();
+        sum = sum+num[i];
       }
-      for(i=0;i<=10;i++){
-        System.out.println(marks[i]);
-      }
+        
+        avg=sum/10;
+        for(i=0;i<=9;i++)
+            System.out.println(num[i]);
+        System.out.println(sum);
+        System.out.println(avg);
+        
+           
+
         
       
     }
