31ce19e954dd5b652048ba86c144886dc9154f4d
Kevin Muthami <muthamikevin80@gmail.com>
2024-03-26 09:46:02 +0300

    update

---
+++ code/divisbility.java
@@ -0,0 +1,11 @@
+package code;
+
+public class divisbility {
+    public static void main(String[]args){
+        int i;
+        for(i=100; i<=400; i++ )
+            if(i%6==0)
+            System.out.println(i + "");
+        
+    }
+}
